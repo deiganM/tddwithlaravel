af679875a648e9f960a12a0b4e2e50aefee35262
Deigan Millikin <dmillikin@fieldsafesolutions.com>
2021-01-06 19:39:00 -0700

    Start of #9

--- tests/Feature/CreatePostTest.php
+++ tests/Feature/CreatePostTest.php
@@ -16,5 +16,23 @@ class CreatePostTest extends TestCase
     {
         // check if the post table is empty
         $this->assertEquals(0, Post::count());
+
+        // dummy data
+        $dummyData = [
+            'title' => 'The quick brown fox',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio fuga earum pariatur? Aliquam voluptatibus velit voluptas deserunt facere sapiente incidunt quas ex soluta totam.'
+        ];
+
+        $this->postJson(routes('posts.store'), $dummyData)->assertStatus(201);
+
+        $this->assertEquals(1, Post::count());
+
+        // check if what we sent was what we saved
+        $post = Post::first();
+
+        $this->assertEquals($dummyData['title'], $post->title);
+        $this->assertEquals($dummyData['body'], $post->body);
+
+        // Trust but verify
     }
 }
